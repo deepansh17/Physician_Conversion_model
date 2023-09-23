0cfdfed89c384282c8bf34a33d1d8eadd1ae545c
deepansh17 <58938351+deepansh17@users.noreply.github.com>
2023-09-23 17:48:27 +0530

    run8

--- setup.py
+++ setup.py
@@ -9,27 +9,13 @@ from setuptools import find_packages, setup
 from physician_conversion_model import __version__
 
 PACKAGE_REQUIREMENTS = ["pyyaml"]
-
 # packages for local development and unit testing
 # please note that these packages are already available in DBR, there is no need to install them on DBR.
-LOCAL_REQUIREMENTS = [
-    "scikit-learn",
-    "pandas"
-]
 
-TEST_REQUIREMENTS = [
-    # development & testing tools
-    "pytest",
-    "coverage[toml]",
-    "pytest-cov"
-]
 
 setup(
     name="physician_conversion_model",
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    setup_requires=["setuptools","wheel"],
     install_requires=PACKAGE_REQUIREMENTS,
-    extras_require={"local": LOCAL_REQUIREMENTS, "test": TEST_REQUIREMENTS},
     entry_points = {
         "console_scripts": [
             "feature_pipeline = physician_conversion_model.tasks.feature_pipeline:entrypoint"
