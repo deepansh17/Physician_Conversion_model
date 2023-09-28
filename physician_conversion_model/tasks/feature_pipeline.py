de4f9510a3e449cf3606ae5a1e20e63ef0a7631c
deepansh17 <58938351+deepansh17@users.noreply.github.com>
2023-09-28 21:43:26 +0530

    run 63

--- physician_conversion_model/tasks/feature_pipeline.py
+++ physician_conversion_model/tasks/feature_pipeline.py
@@ -170,9 +170,9 @@ class DataPrep():
   
     def preprocess_data(self):
         
-        my_module = self.load_module("./physician_conversion_model/tasks/utils.py", "push_df_to_s3")
-                
-        df_input = self.load_data_from_s3()
+        
+        df_input = self.load_module("./physician_conversion_model/tasks/utils.py", "load_data_from_s3")        
+        #df_input = self.load_data_from_s3()
 
         df_input = df_input.reset_index()
 
