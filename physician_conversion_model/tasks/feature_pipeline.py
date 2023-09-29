a52fd300adc9f7f827b7da3f9d5b92ae6db479b8
deepansh17 <58938351+deepansh17@users.noreply.github.com>
2023-09-29 11:21:26 +0530

    final test

--- physician_conversion_model/tasks/feature_pipeline.py
+++ physician_conversion_model/tasks/feature_pipeline.py
@@ -1,119 +1,104 @@
-#system dependencies
 import pandas as pd
 import numpy as np
-
-
-from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
 import warnings
+import yaml
 import os
 import boto3
-import urllib
-import pickle
-import sys
-import os
-
-# Add the project root directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from io import BytesIO
 import importlib.util
 import sys
-#pyspark and feature store 
-import os
-import datetime
 
-#warnings
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, f_classif
+
+from io import BytesIO
+
 warnings.filterwarnings('ignore')
-import yaml 
 
+class DataPrep:
 
-class DataPrep(): 
-    
-    def __init__(self, conf):
-        self.conf = conf
-        
-    def load_module(self,file_name, module_name):
+    def __init__(self, conf_file_path):
+        self.load_configuration(conf_file_path)
+        self.bucket_name = self.conf['s3']['bucket_name']
+        self.aws_region = self.conf['s3']['aws_region']
+        self.file_path = self.conf['s3']['file_path']
+        self.drop_col_list = self.conf['feature_transformation']['drop_column_list']
+        self.encode_col_list = self.conf['feature_transformation']['one_hot_encode_feature_list']
+        self.threshold = self.conf['param_values']['variance_threshold_value']
+        self.n = self.conf['param_values']['select_k_best_feature_num']
+        self.id_col_list = self.conf['feature_transformation']['id_col_list']
+        self.target_col = self.conf['feature_transformation']['target_col']
+        self.folder_path = self.conf['preprocessed']['model_variable_list_file_path']
+        self.file_name = self.conf['preprocessed']['model_variable_list_file_name']
+        self.s3_object_key = self.conf['preprocessed']['preprocessed_df_path']
+
+    def load_module(self, file_name, module_name):
         spec = importlib.util.spec_from_file_location(module_name, file_name)
         module = importlib.util.module_from_spec(spec)
         sys.modules[module_name] = module
         spec.loader.exec_module(module)
-        return module   
-    
-  
-    def preprocess_data(self): 
-        
-        bucket_name = self.conf['s3']['bucket_name']
-        aws_region = self.conf['s3']['aws_region']
-        
-        file_path = self.conf['s3']['file_path']
-        my_module = self.load_module("./physician_conversion_model/tasks/utils.py", "utils")        
+        return module
+
+    def load_configuration(self, conf_file_path):
+        with open(conf_file_path, 'r') as config_file:
+            self.conf = yaml.safe_load(config_file)
+
+    def preprocess_data(self):
+        """
+        Perform data preprocessing steps as specified in the configuration.
+
+        This method loads data from an S3 bucket, cleans the data, performs one-hot encoding,
+        applies feature selection, and saves the preprocessed data back to S3.
+        """
+
+        my_module = self.load_module("./physician_conversion_model/tasks/utils.py", "utils")
         utils_func = my_module.utils()
-        df_input = utils_func.load_data_from_s3(bucket_name,aws_region,file_path)
 
-        df_input = df_input.reset_index()
-
+        df_input = utils_func.load_data_from_s3(self.bucket_name, self.aws_region, self.file_path)
+        
         #Clean column names
         df_input.columns = df_input.columns.str.strip()
         df_input.columns = df_input.columns.str.replace(' ', '_')
-
+        
         #Drop unwanted column: "HCO Affiliation" - "Affiliation Type" is more valid column for us
-        drop_col_list = self.conf['feature_transformation']['drop_column_list']
-        df_input.drop(drop_col_list, axis= 1, inplace= True)
+        df_input.drop(self.drop_col_list, axis=1, inplace=True)
 
         #One hot encode categorical features
-        encode_col_list = self.conf['feature_transformation']['one_hot_encode_feature_list']
-        df_input = pd.get_dummies(df_input, columns=encode_col_list, drop_first=True)
-
+        df_input = pd.get_dummies(df_input, columns=self.encode_col_list, drop_first=True)
+        
         #Select variables for feature selection
         id_target_col_list = self.conf['feature_transformation']['id_target_col_list']
         col_for_feature_selection = df_input.columns.difference(id_target_col_list)
-
+        
         #Variance threshold feature selection method
-        threshold = self.conf['param_values']['variance_threshold_value']
-        var_thr = VarianceThreshold(threshold = threshold) #Removing both constant and quasi-constant
+        var_thr = VarianceThreshold(threshold=self.threshold)
         var_thr.fit(df_input[col_for_feature_selection])
-        #var_thr.get_support()
 
         df_input_subset = df_input[col_for_feature_selection]
-        remove_col_list = [col for col in df_input_subset.columns 
-                        if col not in df_input_subset.columns[var_thr.get_support()]]
+        remove_col_list = [col for col in df_input_subset.columns if col not in df_input_subset.columns[var_thr.get_support()]]
         
         #remove above list column from master dataframe
-        df_input.drop(remove_col_list, axis = 1, inplace = True, errors= 'ignore')
+        df_input.drop(remove_col_list, axis=1, inplace=True, errors='ignore')
 
         #Feature Selection Using Select K Best
-        n = self.conf['param_values']['select_k_best_feature_num']
-        id_col_list = self.conf['feature_transformation']['id_col_list']
-        target_col = self.conf['feature_transformation']['target_col']
-        
-        df = df_input.drop(id_col_list,axis=1)
-        target_col_var = df_input[target_col]
-        top_n_col_list = utils_func.select_kbest_features(
-                df,target_col_var, n)
+        df = df_input.drop(self.id_col_list, axis=1)
+        target_col_var = df_input[self.target_col]
+        top_n_col_list = utils_func.select_kbest_features(df, target_col_var, self.n)
         
         #Convert to list
         top_n_col_list = top_n_col_list.tolist()
 
         # Dump top_n_col_list to s3 bucket
-        folder_path = self.conf['preprocessed']['model_variable_list_file_path']
-        file_name = self.conf['preprocessed']['model_variable_list_file_name']
-        utils_func.pickle_dump_list_to_s3(top_n_col_list,folder_path,file_name,bucket_name,aws_region)
-        
+        utils_func.pickle_dump_list_to_s3(top_n_col_list, self.folder_path, self.file_name, self.bucket_name, self.aws_region)
+
         #column list for dataframe
-        cols_for_model_df_list = id_col_list + top_n_col_list
+        cols_for_model_df_list = self.id_col_list + top_n_col_list
         df_feature_eng_output = df_input[cols_for_model_df_list]
         df_model_input = df_feature_eng_output.copy()
-        s3_object_key = self.conf['preprocessed']['preprocessed_df_path'] 
-        
-        push_status = utils_func.push_df_to_s3(df_model_input,bucket_name,aws_region,file_path,s3_object_key)
+
+        push_status = utils_func.push_df_to_s3(df_model_input, self.bucket_name, self.aws_region, self.file_path, self.s3_object_key)
         print(push_status)
 
-
 if __name__ == '__main__':
-    with open('./conf/tasks/feature_pipepline.yml', 'r') as config_file:
-        configuration = yaml.safe_load(config_file)
-    task = DataPrep(configuration)
+    config_file_path = './conf/tasks/feature_pipeline.yml'
+    task = DataPrep(config_file_path)
     task.preprocess_data()
--- physician_conversion_model/tasks/utils.py
+++ physician_conversion_model/tasks/utils.py
@@ -1,79 +1,62 @@
-#Dependencies
 import boto3
 import urllib
 import pandas as pd
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
-
+from sklearn.feature_selection import SelectKBest, f_classif
 from io import BytesIO
-import uuid
 import pickle
 import os
 import importlib.util
 import sys
 
-def load_module(file_name, module_name):
-        spec = importlib.util.spec_from_file_location(module_name, file_name)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[module_name] = module
-        spec.loader.exec_module(module)
-        return module  
-
-common_module = load_module("./physician_conversion_model/common.py", "common") 
-task_module = common_module.Task()
-
-
-
-class utils(task_module):
+class utils():
    
 
-    def push_df_to_s3(self,df,bucket_name,aws_region,file_path,s3_object_key):
-        bucket_name = self.conf['s3']['bucket_name']
+    def push_df_to_s3(self, df, bucket_name, aws_region, file_path, s3_object_key):
+        """
+        Push a DataFrame to an S3 bucket.
+
+        Args:
+            df (pd.DataFrame): The DataFrame to push to S3.
+            bucket_name (str): The name of the S3 bucket.
+            aws_region (str): The AWS region of the S3 bucket.
+            file_path (str): The path to the S3 file.
+            s3_object_key (str): The key of the S3 object.
+
+        Returns:
+            dict: A dictionary with a status message.
+        """
         aws_access_key = os.environ.get("AWS_ACCESS_KEY_ID")
         aws_secret_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
 
         csv_buffer = BytesIO()
         df.to_csv(csv_buffer, index=False)
         csv_content = csv_buffer.getvalue()
 
-        s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
-                    aws_secret_access_key=aws_secret_key, 
-                    region_name=aws_region)
+        s3 = boto3.resource("s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=aws_region)
 
-        
         s3.Object(bucket_name, s3_object_key).put(Body=csv_content)
 
         return {"df_push_status": 'success'}
     
 
-    def load_data_from_s3(self,bucket_name,aws_region,file_path):
+    def load_data_from_s3(self, bucket_name, aws_region, file_path):
+        """
+        Load data from an S3 bucket.
 
-        # AWS credentials and region
-        
+        Args:
+            bucket_name (str): The name of the S3 bucket.
+            aws_region (str): The AWS region of the S3 bucket.
+            file_path (str): The path to the S3 file.
 
-        
-
+        Returns:
+            pd.DataFrame: The loaded DataFrame.
+        """
         aws_access_key = os.environ.get("AWS_ACCESS_KEY_ID")
         aws_secret_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-        
-        
-        access_key = aws_access_key 
-        secret_key = aws_secret_key
 
-        print(f"Access key and secret key are {access_key} and {secret_key}")
-
-        
-        
-        encoded_secret_key = urllib.parse.quote(secret_key,safe="")
-
-        s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
-                      aws_secret_access_key=aws_secret_key, 
-                      region_name=aws_region)
-                
+        s3 = boto3.resource("s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=aws_region)
 
         s3_object = s3.Object(bucket_name, file_path)
-        
         csv_content = s3_object.get()['Body'].read()
 
         df_input = pd.read_csv(BytesIO(csv_content))
@@ -81,61 +64,48 @@ class utils(task_module):
         return df_input
     
 
-    def select_kbest_features(self, df, target_col,n):
+    def select_kbest_features(self, df, target_col, n):
         """
-        Selects the top n features from the DataFrame using the SelectKBest algorithm.
+        Select the top n features from a DataFrame using the SelectKBest algorithm.
 
         Args:
-            df: The DataFrame to select features from.
-            n: The number of features to select.
+            df (pd.DataFrame): The DataFrame to select features from.
+            target_col (pd.Series): The target column for feature selection.
+            n (int): The number of features to select.
 
         Returns:
-            A list of the top n features.
+            pd.Index: A list of the top n features.
         """
-
-
-        selector = SelectKBest(k=n)
-        selected_features = selector.fit_transform(df, target_col)
-        
+        selector = SelectKBest(score_func=f_classif, k=n)
+        selected_features = selector.fit(df, target_col)
         mask = selector.get_support()
         top_n_features = df.columns[mask]
 
         return top_n_features
+
         
         
-    def pickle_dump_list_to_s3(self, column_list,folder_path,file_name,bucket_name,aws_region):
+    def pickle_dump_list_to_s3(self, column_list, folder_path, file_name, bucket_name, aws_region):
         """
-        Pickle dump a list of columns and upload it to an S3 bucket in the specified folder.
+        Pickle dump a list of columns and upload it to an S3 bucket.
 
         Args:
-        - column_list: List of columns to pickle.
-
-        Returns:
-        - upload pickle list to s3
+            column_list (list): List of columns to pickle.
+            folder_path (str): The path within the S3 bucket where the file will be stored.
+            file_name (str): The name of the pickled file.
+            bucket_name (str): The name of the S3 bucket.
+            aws_region (str): The AWS region of the S3 bucket.
         """
-        # AWS details
-
-        
-
         aws_access_key = os.environ.get("AWS_ACCESS_KEY_ID")
         aws_secret_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-        access_key = aws_access_key 
-        secret_key = aws_secret_key
-        print(f"Access key and secret key are {access_key} and {secret_key}")
-
-        # Create an S3 client
-        s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
-                      aws_secret_access_key=aws_secret_key, 
-                      region_name=aws_region)
 
         # Pickle dump the list
         with open(file_name, 'wb') as file:
             pickle.dump(column_list, file)
 
         # Upload the pickled file to S3
-        s3.Bucket(bucket_name).upload_file(file_name, folder_path + file_name)
-
-        print(f"Pickled file '{file_name}' uploaded to S3 bucket '{bucket_name}' in folder '{folder_path}'.")
+        s3 = boto3.resource("s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=aws_region)
+        s3.Bucket(bucket_name).upload_file(file_name, os.path.join(folder_path, file_name))
 
         
 
