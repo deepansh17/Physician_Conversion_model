51f2e720d3e24cb1256beef3af222f09878a1b62
deepansh17 <58938351+deepansh17@users.noreply.github.com>
2023-10-06 17:46:43 +0530

    inf run 4

--- physician_conversion_model/tasks/inference_pipeline.py
+++ physician_conversion_model/tasks/inference_pipeline.py
@@ -8,7 +8,7 @@ import hopsworks
 import importlib.util
 import sys
 import yaml
-import mlflow
+import mlflow.pyfunc
 
 class Inferencemodel():
     def __init__(self, conf):
@@ -32,7 +32,8 @@ class Inferencemodel():
         df_input = utils_func.load_data_from_s3(self.bucket_name, self.aws_region, self.inference_path)
         df_inference = df_input.drop(self.id_drop_column_list, axis=1)
         df_inference = df_inference.drop(["TARGET"], axis=1)
-        model = mlflow.load_model(registered_model_name="xgboost-model")
+        model_uri = "runs:/84748cf8a31f47f0810daeb43e9f15d1/model"
+        model = mlflow.pyfunc.load_model(model_uri)
         
 if __name__ == '__main__':
     with open('./conf/tasks/feature_pipepline.yml', 'r') as config_file:
